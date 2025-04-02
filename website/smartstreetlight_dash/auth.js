74ad2ead2e815d5a91fe8d97eb1c1520161b722c
alvt55 <122847548+alvt55@users.noreply.github.com>
2025-04-02 11:20:13 -0700

    draft: added TODO item for the future

--- website/smartstreetlight_dash/auth.js
+++ website/smartstreetlight_dash/auth.js
@@ -1,7 +1,10 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
 })
+
+// TODO: use middleware and callbacks to guard api calls
+// https://authjs.dev/getting-started/session-management/protecting?framework=Next.js%2520%28Client%29
